9c6681287940944c1874943bfa6e217f2ab99c75
¨danickblouin¨ <danick.blouin1@gmail.com>
2022-12-03 12:45:31 -0500

    Day 02 Part 2

--- day02/day02.py
+++ day02/day02.py
@@ -1,3 +1,26 @@
+# NEED TO LEARN HOW TO USE PYTHON PROPERLY
+from enum import Enum
+
+class Item(Enum):
+    ROCK = "A"
+    PAPER = "B"
+    SCISSORS = "C"
+
+class Points(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+class ToDo(Enum):
+    LOOSE = "X"
+    DRAW = "Y"
+    WIN = "Z"
+
+class Points(Enum):
+    LOOSE = 0
+    DRAW = 3
+    WIN = 6
+
 # A = Rock
 # B = Paper
 # C = Scissors
@@ -49,22 +72,39 @@ def part_one(path):
             print ("TEST" + point)
     print("PART ONE:" + str(point))
 
-# # X = loose
-# # Y = draw
-# # Z = win
-# def part_two(path):
-#     with open(path) as file:
-#         lines = [line.strip() for line in file.readlines()]
-#     for line in lines:
-#         two = line.split(" ")
-#         other = two[0]
-#         me = two[1]
-#         point = 0
-#         if other == "A":
-#             if me == "X"
-#         elif other == "B":
-#         elif other == "C":
-#     print("PART TWO:" + str(point))
+def part_two(path):
+    with open(path) as file:
+        lines = [line.strip() for line in file.readlines()]
+
+    point = 0
+    for line in lines:
+        two = line.split(" ")
+        other = two[0]
+        toplay = two[1]
+        if toplay == ToDo.LOOSE.value:
+            if other == Item.ROCK.value:
+                point += 3
+            elif other == Item.PAPER.value:
+                point += 1
+            else:
+                point += 2
+        elif toplay == ToDo.DRAW.value:
+            point += Points.DRAW.value
+            if other == Item.ROCK.value:
+                point += 1
+            elif other == Item.PAPER.value:
+                point += 2
+            else:
+                point += 3
+        elif toplay == ToDo.WIN.value:
+            point += Points.WIN.value
+            if other == Item.ROCK.value:
+                point += 2
+            elif other == Item.PAPER.value:
+                point += 3
+            else:
+                point += 1
+    print("PART TWO:" + str(point))
 
 if __name__ == "__main__":
     input_path = "./day02/input.txt"
