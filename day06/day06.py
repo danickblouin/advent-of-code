ff186cf32ec644ad9f42617ea36630a31460970d
¨danickblouin¨ <danick.blouin1@gmail.com>
2022-12-06 10:33:06 -0500

    Day06 cleanup

--- day06/day06.py
+++ day06/day06.py
@@ -1,23 +1,16 @@
 def unique(s):
     return len(set(s)) == len(s)
 
-def part_one(lines):
+def main(lines,size):
     for x,char in enumerate(line):
-        four = line[x:x+4]
-        if unique(four):
-            return line.index(four)+4
-
-def part_two(lines):
-    for x,char in enumerate(line):
-        fourteen = line[x:x+14]
-        if unique(fourteen):
-            return line.index(fourteen)+14
+        text = line[x:x+size]
+        if unique(text):
+            return line.index(text)+size
 
 if __name__ == "__main__":
-    input_path = "./day06/input.txt"
-    with open(input_path) as file:
+    with open("input.txt") as file:
         lines = [line.strip() for line in file.readlines()]
         for line in lines:
-            print("PART ONE: {}".format(part_one(line)))
-            print("PART TWO: {}".format(part_two(line)))
+            print("PART ONE: {}".format(main(line,4)))
+            print("PART TWO: {}".format(main(line,14)))
             break
