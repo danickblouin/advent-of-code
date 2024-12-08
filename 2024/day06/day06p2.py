40471c50fb3aa874909b2a8291cfb1941b61f10d
danickblouin <danick.blouin1@gmail.com>
2024-12-08 18:37:04 -0500

    2024 - Day 06 - redid part 2 by myself

--- 2024/day06/day06p2.py
+++ 2024/day06/day06p2.py
@@ -1,76 +1,83 @@
-# got help from my friend chatgpt for this one
-# it's based on my part 1 tho
-lines = [list(line.rstrip()) for line in open('input.txt').readlines()]
+# there's some performance gain possible by not editing the grid
+
+lines = [line.rstrip() for line in open('input.txt').readlines()]
+
+lines = [list(line) for line in lines]
 
 guards = "^>v<"
-dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+lines_backup = [line[:] for line in lines]
 
 
-def get_start(lines):
+def get_pos(lines):
     for row in range(len(lines)):
         for col in range(len(lines[row])):
             if lines[row][col] in guards:
-                return row, col, guards.index(lines[row][col])
-    return None, None, None
+                return [row, col]
+    return [0, 0]
 
 
-start_row, start_col, start_dir = get_start(lines)
-
-# Convert the guard's initial position to floor '.' and just store the position and direction separately
-lines[start_row][start_col] = '.'
+dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
 
-def simulate(l, obstacle_row=None, obstacle_col=None):
-    # If we place an obstacle, do it now
-    if obstacle_row is not None and obstacle_col is not None:
-        original_char = l[obstacle_row][obstacle_col]
-        l[obstacle_row][obstacle_col] = '#'
-    else:
-        original_char = None
+def get_dir(l, row, col):
+    guard = l[row][col]
+    return dirs[guards.index(guard)]
 
-    visited_states = set()
-    # Guard's starting position and direction
-    gr, gc, gdir = start_row, start_col, start_dir
 
-    while True:
-        state = (gr, gc, gdir)
-        if state in visited_states:
-            # Loop detected
-            result = True
-            break
-        visited_states.add(state)
+l_pos = []
 
-        # Check if guard is at the boundary
-        if gr == 0 or gr == len(l) - 1 or gc == 0 or gc == len(l[0]) - 1:
-            # Guard leaves the map
-            result = False
-            break
-
-        d_row, d_col = dirs[gdir]
-        front_cell = l[gr + d_row][gc + d_col]
-
-        if front_cell == '#':
-            # Turn right
-            gdir = (gdir + 1) % 4
-            # Don't move, just change direction
+guard_pos = get_pos(lines)
+while True:
+    row, col = guard_pos[0], guard_pos[1]
+    if row in [0, len(lines) - 1] or col in [0, len(lines[row]) - 1]:
+        break
+    direction = get_dir(lines, row, col)
+    d_row, d_col = direction[0], direction[1]
+    if lines[row + d_row][col + d_col] == "#":
+        if guards.index(lines[row][col]) + 1 == len(guards):
+            lines[row][col] = guards[0]
         else:
-            # Move forward
-            gr += d_row
-            gc += d_col
+            lines[row][col] = guards[guards.index(lines[row][col]) + 1]
+        continue
+    else:
+        lines[row + d_row][col + d_col] = lines[row][col]
+        guard_pos = [row + d_row, col + d_col]
 
-    # Revert the obstacle if placed
-    if obstacle_row is not None and obstacle_col is not None:
-        l[obstacle_row][obstacle_col] = original_char
+    lines[row][col] = "X"
+    if [row, col] not in l_pos:
+        l_pos.append([row,col])
 
-    return result
+l_pos.append(guard_pos)
+
+total = 0
+
+for pos in l_pos[1:]:
+    row_pos, col_pos = pos[0], pos[1]
+    lines_copy = [line[:] for line in lines_backup]
+    lines_copy[row_pos][col_pos] = '#'
+    switch_pos = []
+    guard_pos = get_pos(lines_copy)
+    while True:
+        row, col = guard_pos[0], guard_pos[1]
+        if switch_pos.count([row, col]) > 2:  # in loop
+            total += 1
+            break
+        if row in [0, len(lines_copy) - 1] or col in [0, len(lines_copy[row]) - 1]:
+            break
+        direction = get_dir(lines_copy, row, col)
+        d_row, d_col = direction[0], direction[1]
+        if lines_copy[row + d_row][col + d_col] == "#":
+            if guards.index(lines_copy[row][col]) + 1 == len(guards):
+                lines_copy[row][col] = guards[0]
+            else:
+                lines_copy[row][col] = guards[guards.index(lines_copy[row][col]) + 1]
+                switch_pos.append([row, col])
+            continue
+        else:
+            lines_copy[row + d_row][col + d_col] = lines_copy[row][col]
+            lines_copy[row][col] = "X"
+            guard_pos = [row + d_row, col + d_col]
 
 
-loop_count = 0
-
-for r in range(len(lines)):
-    for c in range(len(lines[r])):
-        if lines[r][c] == '.' and not (r == start_row and c == start_col):
-            if simulate(lines, r, c):
-                loop_count += 1
-
-print(loop_count)
+print(total)
